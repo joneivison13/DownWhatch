38bc26c6776de96b258e7fff7a1bd734fdae733e
joneivison013 <joneivilson@webpeople.net.br>
2020-12-13 16:45:23 -0300

    finalização de tela home

---
+++ src/Components/Button/Components/OpenBrowser/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "./styles";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const OpenBrowser = () => {
+  return (
+    <Button>
+      <Ionicons name="earth" color="#FFF" />
+    </Button>
+  );
+};
+
+export default OpenBrowser;
--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,28 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Image } from "react-native";
 import { Button } from "./styles";
 
 import PlusImage from "../../assets/img/plus.png";
 import { Context } from "../../Context";
+import OpenBrowser from "./Components/OpenBrowser";
 
 export default function ButtomDownload() {
   const context = useContext(Context);
 
+  const [mostOptions, setMostOptions] = useState(false);
+
   if (context.videos.length <= 0) {
     return (
-      <Button
-        style={{
-          borderColor: "#f00",
-        }}
-      >
-        <Image source={PlusImage} />
-      </Button>
-    );
-  } else {
-    return (
-      <Button>
-        <Image source={PlusImage} />
-      </Button>
+      <>
+        {mostOptions && <OpenBrowser />}
+        <Button
+          style={{
+            borderColor: "#f00",
+          }}
+          onPress={() => setMostOptions(true)}
+        >
+          <Image source={PlusImage} />
+        </Button>
+      </>
     );
   }
+  return (
+    <Button onPress={() => setMostOptions(true)}>
+      <Image source={PlusImage} />
+    </Button>
+  );
 }
